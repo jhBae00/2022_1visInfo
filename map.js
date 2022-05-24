e488d4dfd2628e2cacdeb182a74a9ca11580e2be
jhBae00 <88951415+jhBae00@users.noreply.github.com>
2022-05-24 20:44:50 +0900

    Update map.js

--- map.js
+++ map.js
@@ -1,65 +1,44 @@
 
-//Width and height
-let w = 760;
+
+
+let w = 780;
 let h = 600;
 
-//Define map projection
 let projection = d3.geoMercator()
              .center([ -120, 37 ])
-             .translate([ w/2, h/2 ])
-             .scale([ w*3.3 ]);
+             .translate([ 390, 300 ])
+             .scale([ 780*3.3 ]);
 
-//Define path generator
 let path = d3.geoPath()
          .projection(projection);
-
-//Create SVG
+//svg
 let svg = d3.select("#container")
       .append("svg")
-      .attr("width", w * 2)
-      .attr("height", h)
+      .attr("width", 780 * 2)
+      .attr("height", 600)
 
-let mainG = svg.append("g")
-
-let linkG = svg.append("g")
+let geoGraph = svg.append("g")
+let barChart = svg.append("g")
     .attr("transform", "translate(800 10)")
-    .attr("class", "noPointerEvents")
 
 console.log("dddddd")
-console.log(mainG)
+console.log(geoGraph)
 
-// Reference to our controls panel and legend svg in the HTML
-let controls = d3.select("#container").select("#controls")
-let legend = d3.select("#legend")
-
-// Create scales to be used to visualize data. We do not know the domain until we read in data.
+let tasks = d3.select("#container").select("#tasks")
 let facNumDegree = d3.scaleSequential().interpolator(d3.interpolateOrRd)
-// let populationScale = d3.scalePow().range([20, 300]);
-let partyScale = d3.scaleLinear().domain([50,100]).range([0.3,0.95])
+let partyAxisScale = d3.scaleLinear().domain([50,100]).range([0.3,0.95])
 
-// TODO
-// Abstract out functions:
-// ParseData()
-// SetControls()
-//   Toggle Ballot Results - YES(green) or NO(red), size = population, transparency = percentage that voted and won
-//   Set Political Affiliation - Red or blue, shade = percentage, based on governor elections
-//   Set Gas Consumption       - Yellow to red, shade = higher gas consumption, gray = no data
-//   Set Public Transportation -
-// DrawMap()
+
 d3.csv("california-medical-facilitiy-crosswalk.csv").then(function(medFacData) {
+  console.log("merging")
   d3.json("california-county-map.geojson").then(function(countyMap) {
     console.log("jsonparsingOk?")
-    
     for (let i = 0; i < countyMap.features.length; i++) {
-      let abb = countyMap.features[i].properties.NAME;
-      abb = abb.toUpperCase();
-
+      let mapCountyName = countyMap.features[i].properties.NAME;
       console.log("jsonparsingOIs Going on!")
       for(let j = 0; j < medFacData.length; j++) {
-        let bcc =medFacData[j].COUNTY_NAME;
-        if(abb === bcc) {
+        if(medFacData[j].COUNTY_NAME.toUpperCase() == mapCountyName) {
           let regsBoth, regsOnlyOSHPD, statusClosed, statusOpen, statusSuspense, statusUC, size;
-          //countyName = medFacData[j].COUNTY_NAME;
           regedBoth = medFacData[j].both;
           regsOnlyOSHPD = medFacData[j].onlyOSHPD;
           statusClosed = medFacData[j].Closed;
@@ -67,49 +46,46 @@ d3.csv("california-medical-facilitiy-crosswalk.csv").then(function(medFacData) {
           statusSuspense = medFacData[j].Suspense;
           statusUC = medFacData[j].Under_Construction;
           size = medFacData[j].size;
-
           countyMap.features[i].properties.countyName = mapCountyName;
           countyMap.features[i].properties.regsBoth = regesBoth;
           countyMap.features[i].properties.regsOnlyOSHPD = regsOnlyOSHPD;
           countyMap.features[i].properties.statusClosed = statusClosed;
           countyMap.features[i].properties.statusOpen = statusOpen;
+
           countyMap.features[i].properties.statusSuspense = statusSuspense;
-          countyMap.features[i].properties.statusUC = statusUC;
+          countyMap.features[i].properties.statusUC = statusUC
+
           if(size !=true){
             countyMap.features[i].properties.medNum = 0;
           }
           else{
             countyMap.features[i].properties.medNum = size;
           }
+
           break;
-
         }
-
+        break;
       }
     }
 
 
-//// debug
-console.log(countyMap);
 let clicked = [];
-// Calculate the domains of our scales, now that we have the data.
-let gasMin = d3.min(countyMap.features, function(d) { return d.properties.medNum; })
-let gasMax = d3.max(countyMap.features, function(d) { return d.properties.medNum; })
-facNumDegree.domain(d3.extent(countyMap.features, function(d) { return d.properties.medNum; }))
+facNumDegree.domain(d3.extent(countyMap.features, function(d) { return d.properties.ALAND; }))
 
-// Calculate scales of the 2nd visualization (bar graph)
-let xScale = d3.scaleBand()
-      .range([0, w-80])
-      .domain(d3.extent(clicked, function(d) { return d[i] }))
-      .padding(0.2)
+let xAxisScale = d3.scaleBand()
+      .range([0, w -100])
+      .domain(d3.extent(clicked,
+                        function(d) { return d[i] }))
+      .padding(0.3)
 
-let yScale = d3.scaleLinear()
-      .range([h-100, 10])
-      .domain(d3.extent(medFacData, function(d) { return d.size }));
+let yAxisScale = d3.scaleLinear()
+      .range([h - 100, 10])
+      .domain(d3.extent(medFacData,
+                          function(d) { return d.size }));
 
-linkG.append('g')
+barChart.append('g')
       .attr('transform', `translate(0, ${h-100})`)
-      .call(d3.axisBottom(xScale))
+      .call(d3.axisBottom(xAxisScale))
       .selectAll("text")
         .attr("y", 0)
         .attr("x", 9)
@@ -117,29 +93,46 @@ linkG.append('g')
         .attr("transform", "rotate(90)")
         .style("text-anchor", "start");
 
-linkG.append('g')
-      .call(d3.axisLeft(yScale));
+barChart.append('g')
+      .call(d3.axisLeft(yAxisScale));
 
-linkG.append('g')
+barChart.append('g')
       .attr('class', 'grid')
       .call(d3.axisLeft()
-        .scale(yScale)
+        .scale(yAxisScale)
         .tickSize(-w+80, 0, 0)
         .tickFormat('')
       )
 
 
-      let bcc =medFacData[43].COUNTY_NAME
-      let abb = countyMap.features[1].properties.NAME
-      abb = abb.toUpperCase();
-      if(bcc == abb){
-        d = 'cool'
+      let fff =medFacData[43].COUNTY_NAME
+      let ggg = countyMap.features[1].properties.NAME
+      ggg = ggg.toUpperCase();
+      if(fff == ggg){
+        f = 'cool'
       }
       else{
-        d = 'bad'
+        f = 'bad'
       }
 
-linkG.append('text')
+      deluca = medFacData[43].COUNTY_NAME
+
+      for(i=0; i<medFacData.length; i++){
+        let bcc = medFacData[i].COUNTY_NAME;
+        for(j=0; j<medFacData.length; j++){
+          let abb = countyMap.features[j].properties.NAME
+          abb = abb.toUpperCase();
+          if(bcc == abb){
+            d = 'cool'
+          }
+          else{
+            d = 'bad'
+          }
+        }
+      }
+
+
+barChart.append('text')
   .attr('class', 'label')
   .attr('x', -(h/ 2) + 20)
   .attr('y', -50)
@@ -147,7 +140,7 @@ linkG.append('text')
   .attr('text-anchor', 'middle')
   .text('Medical Facitilty Condition'+ d)
 
-  let barGroups = linkG.selectAll()
+  let barGroups = barChart.selectAll()
     .data(medFacData)
     .enter()
     .append('g')
@@ -155,20 +148,17 @@ linkG.append('text')
   barGroups
     .append('rect')
     .attr('class', 'bar')
-    .attr('x', (g) => xScale(clicked))
-    .attr('y', (g) => yScale(medFacData.size))
-    .attr('height', (g) => h - yScale(medFacData.size) -5)
-    .attr('width', xScale.bandwidth())
+    .attr('x', (g) => xAxisScale(clicked))
+    .attr('y', (g) => yAxisScale(medFacData.size))
+    .attr('height', (g) => h - yAxisScale(medFacData.size) -5)
+    .attr('width', xAxisScale.bandwidth())
     .style("fill", "#CCB")
 
 
 
 //// debug
 //console.log(countyMap);
-
-
-// Initialize the main map of California with tooltips giving detailed information per county.
-let mapGraph = mainG.selectAll("path")
+let mapGraph = geoGraph.selectAll("path")
   .data(countyMap.features)
   .enter()
   .append("path")
@@ -185,12 +175,12 @@ let mapGraph = mainG.selectAll("path")
       else {
         clicked.push(d.properties.NAME);
       }
-      mainG.selectAll("path").transition().duration(500).style("fill", function(d) {
+      geoGraph.selectAll("path").transition().duration(500).style("fill", function(d) {
         for(let i = 0; i < clicked.length; i++) {
-          if(clicked[i] == d.properties.NAME.toUpperCase()) {
+          if(clicked[i] == d.properties.NAME) {
             console.log(d.properties.NAME)
             console.log(d.properties.medNum)
-            return facNumDegree(medFacData[].size);
+            return facNumDegree(d.properties.ALAND);
           }
         }
         return '#ccc';
@@ -198,8 +188,7 @@ let mapGraph = mainG.selectAll("path")
 
     })
 
-    // Enables the Gas Consumption Visualization on map.
-    controls.select("#setGasViz").on("click", function() {
+    tasks.select("#status").on("click", function() {
       mapGraph.transition().duration(700)
         .style("fill", function(d) {
         //  if(){
@@ -208,25 +197,16 @@ let mapGraph = mainG.selectAll("path")
         //  else{}
 
         //Get data value
-        let value = d.properties.medNum;
-        console.log(d.properties.medNum)
+        let value = d.properties.ALAND;
         if (value) {
-          //If value exists…
           return facNumDegree(value);
         } else {
-          //If value is undefined…
-          return "#CCC";
+          return "#CCB";
         }
       })
+      //initialize
       clicked = [];
-      linkG.selectAll("rect").attr("stroke", "none")
+      barChart.selectAll("rect").attr("stroke", "none")
     })
-
-
-    // Fire the click event to initalize Gas Visualization
-    //eventFire(document.getElementById('setGasViz'), 'click');
-
-
-    //////////////////
   })
 })
